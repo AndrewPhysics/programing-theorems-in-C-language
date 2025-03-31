ba788be8b6b20f1352a85552edaef0bb15f17ed6
ANDRAS SZABO <szabo.andrew@yahoo.com>
2025-03-31 17:09:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,95 +1,61 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
-
-//auxiliary functions
-void writeArray(int a[], int size)
+//1. decision maker
+bool decisionMaker(int array[], int size);
+//2. sum and average calculation
+int sumOfElements(int array[], int size);
+//3. counting 
+int countingElements(int array[], int size, int numberToCount);
+//4. linear search
+bool P(int x);
+void linearSearch(int array[], int size);
+//select max element
+int selectMax(int array[], int size);
+int main()
 {
-	int i = 0;
-	while (i < size)
+	const int size = 5;
+	int array[size] = { 1,-2,3,4,4 };
+	if (decisionMaker(array, size)) 
 	{
-		a[i] = 1 + rand() % 10;
-		i++;
-	}
-}
-void readArray(int a[], int size)
-{
-	int i = 0;
-	while (i < size)
-	{
-		cout <<i+1<<". " << a[i] << endl;
-		i++;
-	}
-}
-bool P_property(int x)
-{
-	//if (x%3==0)
-	//{
-	// return true;
-	//}
-	//else
-	//{
-	// return false;
-	//}
-	return x % 3 == 0;
-}
-
-//programming theorems
-void variableReplecement(int& a, int& b) 
-{
-	int temp = a;
-	a = b;
-	b = temp;
-}
-int summationOfOneDimArrayElements(int a[], int size)
-{
-	int i = 0;
-	int sum = 0;
-	while (i < size)
-	{
-		sum += a[i];
-		i++;
-	}
-	return sum;
-}
-void decisionMaking(int a[], int size)
-{
-	int i = 0;
-	while (i < size && !P_property(a[i]))
-	{
-		i++;
-	}
-	if (i < size)
-	{
-		cout << "There is an element with P property!" << endl;
+		cout << "There is an element 5 in array!" << endl;
 	}
 	else
 	{
-		cout << "There is NO element with P property!" << endl;
+		cout << "There is no element 5 in array!" << endl;
 	}
+	cout << "Sum of elements: " << sumOfElements(array, size) << endl;
+	cout << "Average of elements: " << (float)sumOfElements(array, size) / (float)size << endl; 
+	cout << "Number of "<< 4 <<" elements in array: " << countingElements(array, size, 4) << endl;
+	linearSearch(array, size);
+	cout << "Max element: " <<selectMax(array, size)<< endl;
 }
-int conditionalSum(int a[], int size)
+bool decisionMaker(int array[], int size)
+{
+	int i = 0;
+	while (i<size && array[i]!=5)
+	{
+		i++;
+	}
+	return i < size;
+}
+int sumOfElements(int array[], int size) 
 {
 	int sum = 0;
 	int i = 0;
-	while (i < size)
+	while (i<size)
 	{
-		if (P_property(a[i]))
-		{
-			sum += a[i];
-		}
+		sum = sum + array[i];
 		i++;
 	}
 	return sum;
 }
-int conditionalCount(int a[], int size)
+int countingElements(int array[], int size, int numberToCount) 
 {
-	int count = 0;
 	int i = 0;
-	while (i < size)
+	int count = 0;
+	while (i<size)
 	{
-		if (P_property(a[i]))
+		if (array[i]==numberToCount)
 		{
 			count++;
 		}
@@ -97,180 +63,42 @@ int conditionalCount(int a[], int size)
 	}
 	return count;
 }
-int conditionalSelect(int a[], int size)
+bool P(int x)
 {
-	int index = -1;
-	//return -1 if P property element not found in array
+	return x > 0;
+}
+void linearSearch(int array[], int size)
+{
+	//Find index of first negativ value.
 	int i = 0;
-	while (i < size)
+	while (i<size && P(array[i]))
 	{
-		if (P_property(a[i]))
-		{
-			index = i;
-			return index;
-		}
 		i++;
 	}
-	return -1;
+	if (i < size) 
+	{
+		cout << "Negativ value found at index: " << i + 1 <<"." << endl;
+	}
+	else
+	{
+		cout << "There is no negativ element found!" << endl;
+	}
 }
-int selectMax(int aa[], int size)
+int selectMax(int array[], int size)
 {
-	int max_i = 0;
+	//get index of max element in array
+	//we assume that the largest element is the zeroth
+	int max = array[0];
 	int i = 0;
-	while (i < size)
+	while (i<size)
 	{
-		if (aa[max_i] < aa[i])
+		if (array[i] > max)
 		{
-			max_i = i;
+			max = array[i];
 		}
 		i++;
 	}
-	return max_i;
-}
-void swapSort(int a[], int size)
-{
-	int i = 0;
-	int j = 0;
-	while (i < size - 1)
-	{
-		j = 0;
-		while (j < size - 1)
-		{
-			if (a[j] > a[j + 1])
-			{
-				int tmp = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = tmp;
-			}
-			j++;
-		}
-		i++;
-	}
-}
-void maxSelectSort(int a[], int size)
-{
-	int tmp;
-	int min;
-	for (int i = 0; i < size - 1; i++)
-	{
-		min = i;
-		for (int j = i + 1; j < size; j++)
-		{
-			if (a[j] < a[min])
-			{
-				min = j;
-			}
-		}
-		if (min != i)
-		{
-			tmp = a[i];
-			a[i] = a[min];
-			a[min] = tmp;
-		}
-	}
-}
-
-void selectionOfElements(int aa[], int size)//ez jó
-{
-	cout << "assortment: " << endl;
-	int* p = new int[size];
-	int numberOfAssortedElements = 0;
-	int j = 0;
-	int i = 0;
-	while (i < size)
-	{
-		if (P_property(aa[i]))
-		{
-			p[j] = aa[i];
-			j++;
-		}
-		else
-		{
-			p[j] = 0;
-		}
-		i++;
-	}
-	if (j == 0)
-	{
-		cout << "There is no element with P_property! " << endl;
-	}
-	numberOfAssortedElements = j;
-	i = 0;
-	while (i < numberOfAssortedElements)
-	{
-		cout << "The selected elements are: " << p[i] << endl;
-		i++;
-	}
-	delete[] p;
-}
-void sectionOfElements()
-{
-	vector<int> a, b, c;
-	int number;
-	
-
+	return max;
 }
 
 
-
-
-
-int main() 
-{
-	//1. variable replecement theorem
-	int a = 10;
-	int b = 20;
-	cout << "a: " << a << ", b: " << b << endl;
-	variableReplecement(a, b);
-	cout << "a: " << a << ", b: " << b << endl;
-	cout << "---" << endl;
-	//---
-	//2. summation theorem, summations of elements in one dimensional array
-	int size = 8;
-	int array[8];
-	writeArray(array, size);
-	readArray(array, size);
-	cout << "---" << endl;
-	int sum=summationOfOneDimArrayElements(array, size);
-	cout << "Sum of elements: " << sum << endl;
-	cout << "---" << endl;
-	//3. decision theorem
-	cout << "P property means that there is an element which is percfecly devisible by 3!" << endl;
-	decisionMaking(array, size);
-	cout << "---" << endl;
-	//4. conditional sum
-	cout << "I summarize only those elements that satisfy property P!" << endl;
-	int P_sum=conditionalSum(array, size);
-	cout << "Sum of elements with P property: " << P_sum << endl;
-	cout << "---" << endl;
-	//5. conditional count
-	int C_sum=conditionalCount(array, size);
-	cout << "Element counting with P property: " << C_sum << endl;
-	cout << "---" << endl;
-	//6. Conditional select
-	int P_index = conditionalSelect(array, size);
-	cout << "Index of first element with P property: " << P_index+1 << endl;
-	cout << "---" << endl;
-	//7. Select max element
-	int M_index = selectMax(array, size);
-	cout << "Index of maximum value element: " << M_index + 1 << endl;
-	cout << "---" << endl;
-	//8. Swap sort in ascending order.
-	swapSort(array, size);
-	readArray(array, size);
-	cout << "---" << endl;
-	writeArray(array, size);
-	cout << "New random numbers!" << endl;
-	readArray(array, size);
-	cout << "---" << endl;
-	//9. Element sorting by maximum select.
-	maxSelectSort(array, size);
-	readArray(array, size);
-	cout << "---" << endl;
-	//10. Selection of elements with P properties
-	selectionOfElements(array, size);
-	cout << "---" << endl;
-	//11. Section of elements
-
-
-}
