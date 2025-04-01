cd0b6255c4b7c4515326fada2e0806544770d2d6
ANDRAS SZABO <szabo.andrew@yahoo.com>
2025-04-01 17:42:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 using namespace std;
+
+//0. Read array
+void readArray(int array[], int size);
 //1. decision maker
 bool decisionMaker(int array[], int size);
 //2. sum and average calculation
@@ -9,12 +12,32 @@ int countingElements(int array[], int size, int numberToCount);
 //4. linear search
 bool P(int x);
 void linearSearch(int array[], int size);
-//select max element
+//5. select max element
 int selectMax(int array[], int size);
+//6. select index of max element
+int selectMaxIndex(int array[], int size);
+//7. swap sorting in ascending order
+void simpleSwapSort(int array[], int size);
+//8. bubble sorting in ascending order
+void bubbleSort(int array[], int size);
+//9. max select sorting in ascending order
+void maxSelectSort(int array[], int size);
+//10.logarithmic search
+
+//11. Kiválagoatás tétele
+
+//12. metszet képzés
+
+//13. unió képzés
+
+
+
 int main()
 {
 	const int size = 5;
-	int array[size] = { 1,-2,3,4,4 };
+	int array[size] = { 1,-2,7,4,4 };
+	readArray(array, size);
+	cout << "--" << endl;
 	if (decisionMaker(array, size)) 
 	{
 		cout << "There is an element 5 in array!" << endl;
@@ -28,6 +51,27 @@ int main()
 	cout << "Number of "<< 4 <<" elements in array: " << countingElements(array, size, 4) << endl;
 	linearSearch(array, size);
 	cout << "Max element: " <<selectMax(array, size)<< endl;
+	cout << "Index of max element: " << selectMaxIndex(array, size)+1<<". " << endl;
+	cout << "--" << endl;
+	readArray(array, size);
+	//simpleSwapSort(array, size);
+	cout << "--" << endl;
+	//readArray(array, size);
+	cout << "--" << endl;
+	//bubbleSort(array, size);
+	//readArray(array, size);
+	cout << "--" << endl;
+	maxSelectSort(array, size);
+	readArray(array, size);
+}
+void readArray(int array[], int size)
+{
+	int i = 0;
+	while (i < size)
+	{
+		cout << array[i] << endl;
+		i++;
+	}
 }
 bool decisionMaker(int array[], int size)
 {
@@ -88,6 +132,7 @@ int selectMax(int array[], int size)
 {
 	//get index of max element in array
 	//we assume that the largest element is the zeroth
+	//if not then rewrite max variable
 	int max = array[0];
 	int i = 0;
 	while (i<size)
@@ -100,5 +145,86 @@ int selectMax(int array[], int size)
 	}
 	return max;
 }
-
+int selectMaxIndex(int array[], int size)
+{
+	//get index of max element in array
+	//we assume that the largest element is the zeroth
+	//if not then rewrite max variable
+	int maxIndex = 0;
+	int i = 0;
+	while (i < size)
+	{
+		if (array[i] > array[maxIndex])
+		{
+			maxIndex = i;
+		}
+		i++;
+	}
+	return maxIndex;
+}
+void simpleSwapSort(int array[], int size) 
+{
+	int i = 0;
+	int j = 0;
+	int tmp = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - 1)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+void bubbleSort(int array[], int size)
+{
+	int i = size - 1;
+	int j = 0;
+	while (i > 0)
+	{
+		j = 0;
+		while (j < i)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int seged = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = seged;
+			}
+			j++;
+		}
+		i--;
+	}
+}
+void maxSelectSort(int array[], int size)
+{
+	int max_index = 0;
+	int i = size - 1;
+	int j = 0;
+	int seged = 0;
+	while (i >= 0)
+	{
+		while (j <= i)
+		{
+			if (array[j] > array[max_index])
+			{
+				max_index = j;
+			}
+			j++;
+		}
+		seged = array[i];
+		array[i] = array[max_index];
+		array[max_index] = seged;
+		i--;
+		max_index = 0;
+		j = 0;
+	}
+}
 
